a5e298ae592dfc05646d2016d5e0951c64cf9f90
gammafer <60357762+gammafer@users.noreply.github.com>
2021-07-29 17:29:52 +0200

    You can choose side and can win, if you have 3 sign in a row and also restart the game any time. AI to be added

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const Gameboard=(()=>{
         let _board;
 
         if(localStorage.getItem('_board')==null){
-
         _board=new Array(9);
 
     }
@@ -17,7 +16,6 @@ const Gameboard=(()=>{
         return _board;
 
     };
-    
     return {toBeOrNotToBe};
 })();
 
@@ -26,11 +24,55 @@ const Player=(playerName,sign)=>{
 }
 
 const GameFlow=(()=>{
-    const testMaker=()=>{
-    let newBoard= Gameboard.toBeOrNotToBe();
+
+    let player;
+    let enemy;
+    let x=document.getElementById('signX');
+    let o=document.getElementById('signO');
+    let restart=document.getElementById('restartBtn');
     const gameScreen=document.getElementById('gameScreen');
     let subDivs=gameScreen.getElementsByTagName('div');
-    console.log(subDivs);
+    
+    const winningCons=[
+        [1,2,3],
+        [1,4,7],
+        [1,5,9],
+        [4,5,6],
+        [7,8,9],
+        [7,5,3],
+        [3,6,9],
+        [2,5,8]
+
+    ];
+
+    const chooseSide=()=>{
+        
+
+        x.addEventListener('click',function(){
+            player=Player('player','X');
+            enemy = Player('enemy', 'O');
+            disableBtns();
+            startGame();
+        });
+
+
+
+        o.addEventListener('click',function(){
+            player=Player('player','O');
+            enemy = Player('enemy', 'X');
+            disableBtns();
+            startGame();
+        });
+        
+    }
+    const disableBtns=()=>{
+        x.disabled = "disabled";
+        o.disabled = "disabled";
+    }
+
+    const startGame=()=>{
+
+    let newBoard= Gameboard.toBeOrNotToBe();
     for(let i=0;i<newBoard.length;i++){
         if(newBoard[i]==undefined){
             newBoard[i]='';
@@ -40,10 +82,49 @@ const GameFlow=(()=>{
         subDivs[i].innerHTML=newBoard[i];
     }
     }
+
+
     const fillArea=(e)=>{
-        e.target.innerHTML='X';
+        if(e.target.innerHTML===''){
+            e.target.innerHTML=player.sign
+            checkWin(player.sign);
+        };     
     }
-    return{testMaker};
+
+
+    const checkWin=(sign)=>{
+        for(let i=0;i<winningCons.length;i++){
+           let winningCon=winningCons[i];
+           let a= subDivs[winningCon[0]-1].innerHTML===sign;
+           let b= subDivs[winningCon[1]-1].innerHTML===sign;
+           let c= subDivs[winningCon[2]-1].innerHTML===sign;
+           if(a && b && c){
+               console.log(sign ," wins");
+               endGame();
+               break;
+           }
+        }
+    }
+
+
+    const restartGame=()=>{
+        localStorage.removeItem('_board');
+        startGame();
+    }
+    
+    const endGame=()=>{
+        for(let i=0;i<subDivs.length;i++){
+            subDivs[i].removeEventListener('click',fillArea);
+        }
+    }
+
+    restart.addEventListener('click',restartGame);
+
+
+    return{chooseSide,startGame};
 })();
 
-GameFlow.testMaker();
+const BtnControl=()=>{
+    
+}
+GameFlow.chooseSide();
